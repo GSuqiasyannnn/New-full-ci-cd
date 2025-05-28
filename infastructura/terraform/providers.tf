e3e2c465071db1a8b51d879b1fbcca48b3aeef6c
gor <gorsukiasyan72@gmail.com>
2025-05-28 12:59:08 +0000

    new

--- infastructura/terraform/providers.tf
+++ infastructura/terraform/providers.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  access_key 		   = var.aws_access_key
-  secret_key 		   = var.aws_secret_key
+  #access_key 		   = var.aws_access_key
+  #secret_key 		   = var.aws_secret_key
   shared_credentials_files = ["/var/lib/jenkins/.aws/credentials"]
 }
